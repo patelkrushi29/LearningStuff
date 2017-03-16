da5db34ab010f0e7ae0e57eaeed70052b0c316e6
patelkrushi29 <patelkrushi29@gmail.com>
2017-03-15 21:25:30 -0400

    Update main.py

--- how-to-construct-classes/python/Calculator/main.py
+++ how-to-construct-classes/python/Calculator/main.py
@@ -6,21 +6,28 @@ class Calculator(object):
     self.clear();
 
   def add(self,operand):
-    raise Exception("Oi! Krushi! Write this code! :D");
+    self.value += operand;
+    return self.value;
+    #raise Exception("Oi! Krushi! Write this code! :D");
 
   def subtract(self,operand):
     self.value -= operand;
     return self.vlaue;
 
   def multiply(self,operand):
-    raise Exception("Oi! Krushi! Write this code! :D");
+    self.value *= operand;
+    return self.value;
+    #raise Exception("Oi! Krushi! Write this code! :D");
 
   def divide(self,operand):
     self.value /= operand;
     return self.vlaue;
 
   def power(self,operand):
-    raise Exception("Oi! Krushi! Write this code! :D");
+    self.value **= operand;
+    return self.value;
+    #Assuming that user always wants x^x. if not we will need two inputs
+    #raise Exception("Oi! Krushi! Write this code! :D");
 
   def clear(self):
     self.value = 0;
